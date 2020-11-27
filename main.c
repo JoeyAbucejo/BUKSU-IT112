ce6200edb74072eaff7f67823c0f8adf0e3501fc
JoeyAbucejo <73929565+JoeyAbucejo@users.noreply.github.com>
2020-11-27 02:18:52 -1200

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 
 int main()
 {
- int jo;
-printf ("enter a number:")
-;scanf("%d",&jo);
+    int angle1, angle2, angle3, sum;
 
-if(jo % 2 == 0)
-    printf("%d is even number.", jo);
-else
-    printf("%d is an odd number.", jo);
-return 0;
+    printf("Input three angles of a triangle: \n");
+    scanf("%d%d%d", &angle1, &angle2, &angle3);
 
+    sum = angle1 + angle2 + angle3;
+
+    if(sum == 180 && angle1 > 0 && angle2 > 0 && angle3 > 0)
+    {
+        printf("Triangle is valid");
+    }
+    else
+    {
+        printf("Triangle is not valid");
+    }
+    return 0;
 }
