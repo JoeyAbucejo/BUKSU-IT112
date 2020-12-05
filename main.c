99dd01fd7b04925171d46fded3c855ff217399f3
JoeyAbucejo <73929565+JoeyAbucejo@users.noreply.github.com>
2020-12-05 02:37:33 -1200

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main() {
+int i;
+for (i = 6; i <= 10; ++i)
 {
-    char a;
-    int capital_vowel, dicapital_vowel;
-    printf("Enter a letter from the alphabet: ");
-    scanf("%c", &a);
-
-    capital_vowel = (a == 'A' || a == 'E' || a == 'I' || a== 'O' || a == 'U');
-
-    dicapital_vowel = (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u');
-
-    if (capital_vowel || dicapital_vowel)
-    {
-        printf("%c is a vowel.", a);
-    }
-    else
-    {
-        printf("%c is a consonant.", a);
-    }
-    return 0;
+printf("%d: %d\n", i, i * i);
+}
+return 0;
 }
