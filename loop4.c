15b98aa68c41d48f9b51eb1b71af8884deca4050
JoeyAbucejo <73929565+JoeyAbucejo@users.noreply.github.com>
2020-12-05 02:46:02 -1200

    Add files via upload

---
+++ loop4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,spc,rows,c,d=1;
+   printf("Input number of rows : ");
+   scanf("%d",&rows);
+   spc=rows+4-1;
+   for(a=1;a<=rows;a++)
+   {
+         for(c=spc;c>=1;c--)
+            {
+              printf(" ");
+            }
+	   for(b=1;b<=a;b++)
+	   printf("%d ",d++);
+	printf("\n");
+    spc--;
+   }
+    return 0;
+}
